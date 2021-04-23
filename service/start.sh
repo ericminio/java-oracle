c8de09872c397e950bfd2aacc6a4f90de8813152
Eric Mignot <eric.mignot@gmail.com>
2021-04-22 19:34:46 -0700

    network

--- service/start.sh
+++ service/start.sh
@@ -30,11 +30,11 @@ docker ps -a
 docker network ls
 
 docker rm java7
-docker run --rm --name java7 --network exploration_default java:7 java -version
-docker run --rm --name java7 --network exploration_default java:7 mvn -v
-docker run --rm --name java7 --network exploration_default -v "$DIR:/usr/local/src" --workdir /usr/local/src/service/code  java:7 mvn test
+docker run --rm --name java7 --network java-oracle_default java:7 java -version
+docker run --rm --name java7 --network java-oracle_default java:7 mvn -v
+docker run --rm --name java7 --network java-oracle_default -v "$DIR:/usr/local/src" --workdir /usr/local/src/service/code  java:7 mvn test
 
 docker rm java8
-docker run --rm --name java8 --network exploration_default java:8 java -version
-docker run --rm --name java8 --network exploration_default java:8 mvn -v
-docker run --rm --name java8 --network exploration_default -v "$DIR:/usr/local/src" --workdir /usr/local/src/service/code  java:8 mvn test
+docker run --rm --name java8 --network java-oracle_default java:8 java -version
+docker run --rm --name java8 --network java-oracle_default java:8 mvn -v
+docker run --rm --name java8 --network java-oracle_default -v "$DIR:/usr/local/src" --workdir /usr/local/src/service/code  java:8 mvn test
