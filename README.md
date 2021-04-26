8a6c4e8a63ac10365ab2a115a2a67ab613541ff8
Eric Mignot <eric.mignot@gmail.com>
2021-04-26 08:38:05 -0700

    2 actions

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-[![Build Status](https://github.com/ericminio/java-oracle/actions/workflows/ci.yml/badge.svg)](https://github.com/ericminio/java-oracle/actions)
+[![Build Status](https://github.com/ericminio/java-oracle/actions/workflows/java7.yml/badge.svg)](https://github.com/ericminio/java-oracle/actions)
+[![Build Status](https://github.com/ericminio/java-oracle/actions/workflows/java8.yml/badge.svg)](https://github.com/ericminio/java-oracle/actions)
 
 ```
-$env:DIR=$pwd.Path ; docker-compose up service 
+docker-compose up java7 
+docker-compose up java8 
 ```
---
+++ java/java8/start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function execute {
+    docker exec oracle bash -c "export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe && echo \"$1\" | /u01/app/oracle/product/11.2.0/xe/bin/sqlplus -S SYSTEM/oracle@localhost"
+}
+function executeFile {
+    docker exec oracle bash -c "export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe && /u01/app/oracle/product/11.2.0/xe/bin/sqlplus -S SYSTEM/oracle@localhost < $1"
+}
+
+
+ready=0
+while [ "$ready" != "1" ]
+do
+    echo "waiting for Oracle";
+    execute "select 'yes' as ORACLE_IS_READY from dual;" > /tmp/init.output
+    ready=`grep -- "---" /tmp/init.output | wc -l`
+    sleep 1;
+done;
+echo "Oracle is ready";
+
+executeFile /usr/local/src/service/migrations/1-create-table-event.sql
+executeFile /usr/local/src/service/migrations/2-insert-events.sql
+execute "select * from event;"
+executeFile /usr/local/src/service/migrations/3-create-get-event-count.sql
+execute "select exploration.get_event_count() as count from dual;"
+
+pwd
+ls -la
+docker ps -a
+docker network ls
+
+java -version
+mvn -v
+mvn test
